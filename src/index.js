867f4671c9544b9f28bd01b438a2973b4d721706
juan vento <j.j.v.sevilla@gmail.com>
2017-05-27 20:13:11 -0500

    install firebase dependency and init test code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import './index.scss';
+import * as firebase from 'firebase';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var config = {
+  apiKey: "AIzaSyCoLXd0bkWBvsFE5yPmtWT9i1NnVVbxGTI",
+  authDomain: "cookbook-react-firebase.firebaseapp.com",
+  databaseURL: "https://cookbook-react-firebase.firebaseio.com",
+  projectId: "cookbook-react-firebase",
+  storageBucket: "cookbook-react-firebase.appspot.com",
+  messagingSenderId: "1090474301136"
+};
+firebase.initializeApp(config);
+/*
+  configure firebase rules for test
+  {
+    "rules": {
+      ".read": true,
+      ".write": true
+    }
+  }
+*/
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+);
 registerServiceWorker();
